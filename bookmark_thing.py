7f8fb6330a589c5b96871cdf3022f85f33863ec3
Hugo Alvarado <hugo102@gmail.com>
2016-09-19 23:38:07 -0600

    Add logging, removal of stopwords and tokenization via nltk and stop_words package

--- bookmark_thing.py
+++ bookmark_thing.py
@@ -1,46 +1,17 @@
-import os
 import io
 import json
 import requests
 from bs4 import BeautifulSoup
+from collections import Counter
+import sys
+import codecs
+import logging
+from stop_words import get_stop_words
+from nltk import word_tokenize
 
-
-__path = 'C:\\Users\\Hugo Alvarado\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Bookmarks'
-file = io.open(__path, 'r', encoding='utf-8')
-
-
-class Bookmark(object):
-    def __init__(self):
-        pass
-
-
-class ChromeFSBookmark(object):
-    def __init__(self):
-        # Chrome bookmark path in win 10
-        self.__path = 'C:\\Users\\Hugo Alvarado\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Bookmarks'
-
-        # os.path.isfile
-
-    def contents(self):
-        contents = ''
-
-        if os.path.isfile(self.__path):
-            file = io.open(self.__path, 'r', encoding='utf-8')
-            contents = file.read()
-            file.close()
-
-        return contents
-
-
-# scrappy test spider
-# class BookmarkSpider(scrapy.Spider):
-#     name = "BookmarkSpider" # Name of the Spider, required value
-#     start_urls = ["http://deals.souq.com/ae-en/"]  # The starting url, Scrapy will request this URL in parse
-#
-#
-#     def parse(self, response):
-#         for title in response.css('head > title'):
-#             yield title
+def get_bookmark_file():
+    __path = 'C:\\Users\\Hugo Alvarado\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Bookmarks'
+    return io.open(__path, 'r', encoding='utf-8')
 
 
 
@@ -62,30 +33,52 @@ def get_bookmark_children(node, url_list):
     elif 'url' in node['type']:
         #print(node['url'])
         #print(node)
-        node['name'] = node['name'].encode(encoding='UTF-8')
+        node['name'] = node['name']
         return node
 
 
 
 if __name__ == '__main__':
-    bookmark_data = json.load(file)
 
-    #print(len(bookmark_data['roots']['bookmark_bar']['children'][2]['name']))
-    #print(get_bookmark_children(bookmark_data['roots']))
+    logging.basicConfig(filename='bookmark.log', filemode='w', level=logging.DEBUG, format='%(levelname)s:%(message)s')
+
+    logging.info('Starting bookmark analysis.')
+
+    #avoid encoding warnings
+    if sys.stdout.encoding != 'utf8':
+        sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer, 'strict')
+    if sys.stderr.encoding != 'utf8':
+        sys.stderr = codecs.getwriter('utf8')(sys.stderr.buffer, 'strict')
+
+    bookmark_data = json.load(get_bookmark_file())
 
     bookmarks_list = []
     for key,value in bookmark_data['roots'].items():
         if 'sync_transaction_version' != key:
-            print(get_bookmark_children( value, bookmarks_list))
+            get_bookmark_children( value, bookmarks_list)
 
-   #print(bookmarks_list)
+    logging.info( '%s %s' %('Total bookmarks found: ',len(bookmarks_list)))
+
+    # TODO remove duplicates
+
     bookmarks_list = [{'url':'http://www.sitepoint.com/author/agervasio/'},
                       {'url':'http://docs.python-guide.org/en/latest/dev/virtualenvs/'}]
+
+    stop_words = get_stop_words('en')
+    stop_words.append(None)
+
+    logging.debug('Using stopwords:')
+    logging.debug(stop_words)
 
     for bookmark in bookmarks_list:
         #print(bookmark.keys())
 
-        page = requests.get(bookmark['url'])
+        logging.info(bookmark['url'])
+        try:
+            page = requests.get(bookmark['url'])
+        except:
+            logging.exception(bookmark['url'], 'not found')
+            continue
 
         if 404 == page.status_code:
             print('not found', bookmark['url'])
@@ -96,8 +89,28 @@ if __name__ == '__main__':
 
             soup = BeautifulSoup(page.content, 'html.parser')
 
-            print('title:',soup.title.string.encode(encoding='UTF-8'))
-            print(len(soup.find_all(['p','a','h1','h2','h3','h4'])))
+            logging.debug('title:',soup.title)
+
+            # count words skipping stop words
+            site_words = []
+
+            #tags = soup.find_all(['p', 'a', 'h1', 'h2', 'h3', 'h4'])
+            tags = soup.find_all(['p'])
+
+            for tag in tags:
+                #print(tag)
+                #print(tag.string)
+                if None is tag.string:
+                    continue
+
+                for word in word_tokenize(tag.string.lower()):
+                    if word not in stop_words and len(word) > 2:
+                        site_words.append(word.strip())
+
+            words_by_count = Counter(site_words).most_common(20)
+
+            for word in words_by_count:
+                print(word)
 
 
-            #count words, remove common words
+
